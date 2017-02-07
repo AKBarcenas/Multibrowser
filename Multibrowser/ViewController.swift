a324368455025747ad84a677e6c4073332674a39
AKBarcenas <akbarcen@ucsd.edu>
2017-02-06 18:42:57 -0800

    Set up the view controller.

--- Multibrowser/ViewController.swift
+++ Multibrowser/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var addressBar: UITextField!
+    @IBOutlet weak var stackView: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
